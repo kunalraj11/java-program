0a81a5f5b3ab0e39b66872482b3d9437810c046f
kunal raj <qoqoraj123@gmail.com>
2022-11-19 12:57:55 +0530

    second

--- abc.java
+++ abc.java
@@ -2,11 +2,26 @@ import java.util.*;
 public class abc {
     public static void main(String[] args) {
         String name;
+        String c;
         int len;
 
         Scanner obj=new Scanner(System.in);
         System.out.println("Write a sentence");
         name=obj.nextLine();
+        name= name+" ";
+        len=name.length();
+        int pos=0;
+        for (int i=0;i<len;i++) {
+            char b=name.charAt(i);
+            if (b==' ') {
+                c=name.substring(pos,i);
+                pos=i+1;
+                char p = c.charAt(0);
+                if (p =='a' || p =='A' || p =='e' || p =='E' || p =='i' || p =='I' || p =='o' || p =='O' || p =='u' || p =='U') {
+                System.out.println(c);
+                }
+            }
+        }
 
     }
 }
