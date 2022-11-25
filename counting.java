950825a0a6f7bd24313f255824f8599fb5998f52
kunal raj <qoqoraj123@gmail.com>
2022-11-25 14:29:25 +0530

    first

---
+++ counting.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class counting {
+    public static void main(String[] args) {
+        int a;
+        Scanner obj=new Scanner(System.in);
+        System.out.print("enter the number");
+        a=obj.nextInt();
+        for (int i=1;i<=a;i++)
+            System.out.println(i);
+        
+    }
+}
